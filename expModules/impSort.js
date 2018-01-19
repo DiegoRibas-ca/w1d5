f8e591b5a58d34ca99f688a6e53b031960acc898
DiegoRibas-ca <diegoribas.ca@gmail.com>
2018-01-19 18:40:09 +0000

    exercise export add list and sort

---
+++ expModules/impSort.js
@@ -0,0 +1,9 @@
+var importAddSort = require('./sortExport');
+
+importAddSort.addList(3);
+importAddSort.addList(4);
+importAddSort.addList(5);
+importAddSort.addList(8);
+importAddSort.addList(10);
+
+console.log(importAddSort.addList(12));
